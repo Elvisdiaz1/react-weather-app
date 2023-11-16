42d7ccd64d9b673a8fb4f5fadfb9a7e2d5af3ff8
Elvis Diaz <elvisdiaz020@gmail.com>
2023-11-16 18:52:21 -0500

    adds env.example file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./style.css";
 
 // let api = `https://api.openweathermap.org/data/2.5/weather?q={City}&appid={apikey}`
+let api = `https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=process.env.REACT_APP_API_KEY`;
+console.log(api);
 
 function App() {
   return (
